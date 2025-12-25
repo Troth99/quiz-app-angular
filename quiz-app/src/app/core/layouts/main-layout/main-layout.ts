19cbbe0f1254d366ab6dfef12c13f36deda880dd
Troth99 <trothlol123456@gmail.com>
2025-12-26 01:27:53 +0200

    Add achievement toast notification to main layout
    Integrated the achievement toast notification component into the main layout. Added logic to display the toast when an achievement is unlocked and auto-hide it after 4 seconds. Enabled passing the achievement name and handling manual close events from the toast notification.

--- quiz-app/src/app/core/layouts/main-layout/main-layout.ts
+++ quiz-app/src/app/core/layouts/main-layout/main-layout.ts
@@ -2,14 +2,22 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 
 import { QuizFooterComponent, QuizNavigationComponent } from '../../../shared';
+import { AchievementToastNotification } from "../../../features/achievements/achievement-toast-notification/achievement-toast-notification";
 
 
 @Component({
   selector: 'app-main-layout',
-  imports: [QuizNavigationComponent, QuizFooterComponent, RouterOutlet],
+  imports: [QuizNavigationComponent, QuizFooterComponent, RouterOutlet, AchievementToastNotification],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.css'
 })
 export class MainLayout {
+  toastVisible = false;
+  unlockedAchievementName= '';
 
+    showAchievementToast(name: string) {
+    this.unlockedAchievementName = name;
+    this.toastVisible = true;
+    setTimeout(() => this.toastVisible = false, 4000);
+  }
 }
--- quiz-app/src/app/features/achievements/achievement-toast-notification/achievement-toast-notification.ts
+++ quiz-app/src/app/features/achievements/achievement-toast-notification/achievement-toast-notification.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-achievement-toast-notification',
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './achievement-toast-notification.css'
 })
 export class AchievementToastNotification {
+@Input() achievementName: string = "";
+@Output() closed = new EventEmitter<void>()
 
+
+closeToast() {
+  this.closed.emit()
+}
 }
